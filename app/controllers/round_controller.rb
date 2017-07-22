099b961cc66b8e64752b7bdd14059fc721a793c8
Catherin Jean <catherinejeanbean@gmail.com>
2017-07-22 01:10:40 -0500

    psuedo post and puts does not work in routes

--- app/controllers/round_controller.rb
+++ app/controllers/round_controller.rb
@@ -1,8 +1,19 @@
 get '/round/:id' do
-  puts "Am i really here !?!?!?!?!?!?!?!?!?!!!!!!!!!!!!!!!!!!"
+  # puts "Am i really here !?!?!?!?!?!?!?!?!?!!!!!!!!!!!!!!!!!!" puts does not work
   deck_id = params[:id]
   user_id = session[:user_id]
   round = Round.get(user_id,deck_id)
   @guess = round.question
   erb :"round/show"
+end
+
+post '/round/:id' do 
+	#check result against correct answer
+	#correct or wrong
+	#
+	"Display next card"
+
+	# :erb /round/:id/show_result # needs to show the result of the guess 
+	# 
+
 end
